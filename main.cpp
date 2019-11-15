13fe166d68220b88cef2a21e34effe9751c2dbe6
marshl <marshl@live.com.au>
2019-11-15 23:29:11 +1100

    Moved the "RainbowRenderer" into its own class/file

--- main.cpp
+++ main.cpp
@@ -11,90 +11,7 @@
 #include "colour.h"
 #include "pixel.h"
 #include "point.h"
-
-auto rng = std::default_random_engine(std::random_device{}());
-
-enum StartType {
-    START_TYPE_CENTRE,
-    START_TYPE_CORNER,
-    START_TYPE_RANDOM,
-    START_TYPE_EDGE
-};
-
-int PIXELS_WIDE = 256;
-int PIXELS_HIGH = 256;
-int COLOUR_DEPTH = 256;
-
-std::vector<Colour> colours;
-std::vector<Pixel> pixels;
-std::list<Point> available_edges;
-std::vector<Point> neighbours;
-long colour_index = 0;
-
-/// Get the pixel at the given x and y coordinates
-/// \param x The x coordinate
-/// \param y The y coordinate
-/// \return The pixel at that position
-Pixel *getPixel(int x, int y) {
-    return &pixels[y * PIXELS_WIDE + x];
-}
-
-/// Gets the pixel at the given point
-/// \param point The point
-/// \return The pixel at that point
-Pixel *getPixelAtPoint(Point &point) {
-    return &pixels[point.y * PIXELS_WIDE + point.x];
-}
-
-/// Gets the neighbour positions of the given point (points over the edge of the board aren ot returned)
-/// \param point The point to find the neighbours for
-/// \return The number of neighbours
-int getNeighboursOfPoint(Point point) {
-    neighbours.clear();
-    for (int y = -1; y <= 1; ++y) {
-        if (y + point.y < 0 || y + point.y >= PIXELS_HIGH) {
-            continue;
-        }
-
-        for (int x = -1; x <= 1; ++x) {
-            if ((x == 0 && y == 0) || x + point.x < 0 || x + point.x >= PIXELS_WIDE) {
-                continue;
-            }
-
-            neighbours.emplace_back(Point(x + point.x, y + point.y));
-        }
-    }
-    return neighbours.size();
-}
-
-/// Fills the list of random colours
-/// \param colour_depth The number of each unique colours in each channel
-/// \return The total number of colours
-int fillColours(int colour_depth) {
-    for (int r = 0; r < colour_depth; ++r) {
-        for (int g = 0; g < colour_depth; ++g) {
-            for (int b = 0; b < colour_depth; ++b) {
-                colours.emplace_back(Colour(r * 255 / (colour_depth - 1),
-                                            g * 255 / (colour_depth - 1),
-                                            b * 255 / (colour_depth - 1)));
-            }
-        }
-    }
-    std::shuffle(std::begin(colours), std::end(colours), rng);
-    return colours.size();
-}
-
-
-
-/// Fills the pixel at the given point
-/// \param point The pointto place the pixel at
-void fillPoint(Point &point) {
-    available_edges.push_back(point);
-    Pixel *centre_pixel = getPixelAtPoint(point);
-    centre_pixel->colour = colours[colour_index];
-    centre_pixel->is_filled = true;
-    ++colour_index;
-}
+#include "rainbow_renderer.h"
 
 /// Program entrypoint
 /// \param argc
@@ -102,37 +19,44 @@ void fillPoint(Point &point) {
 /// \return
 int main(int argc, char *argv[]) {
 
-    auto difference_func = getColourAbsoluteDiff;
     opterr = 0;
-    StartType start_type = START_TYPE_CENTRE;
-    int num_start_points = 1;
+    RainbowRenderer::StartType start_type = RainbowRenderer::START_TYPE_CENTRE;
+
+    auto rainbow_renderer = new RainbowRenderer();
 
     int c;
-    while ((c = getopt(argc, argv, "h:w:c:d:s:")) != -1)
+    while ((c = getopt(argc, argv, "h:w:c:d:s:")) != -1) {
         switch (c) {
-            case 'w':
-                PIXELS_WIDE = (int) strtol(optarg, nullptr, 0);
-                if (PIXELS_WIDE <= 0) {
+            case 'w': {
+                int pixelsWide = (int) strtol(optarg, nullptr, 0);
+                if (pixelsWide <= 0) {
                     std::cout << "Invalid width argument " << optarg << std::endl;
                     return 1;
                 }
+                rainbow_renderer->setPixelsWide(pixelsWide);
                 break;
-            case 'h':
-                PIXELS_HIGH = (int) strtol(optarg, nullptr, 0);
-                if (PIXELS_HIGH <= 0) {
+            }
+            case 'h': {
+                int pixelsHigh = (int) strtol(optarg, nullptr, 0);
+                if (pixelsHigh <= 0) {
                     std::cout << "Invalid height argument " << optarg << std::endl;
                     return 1;
                 }
+                rainbow_renderer->setPixelsHigh(pixelsHigh);
                 break;
-            case 'c':
-                COLOUR_DEPTH = (int) strtol(optarg, nullptr, 0);
-                if (COLOUR_DEPTH <= 0) {
+            }
+            case 'c': {
+                int colour_depth = (int) strtol(optarg, nullptr, 0);
+                if (colour_depth <= 0) {
                     std::cout << "Invalid colour depth argument " << optarg << std::endl;
                     return 1;
                 }
+                rainbow_renderer->setColourDepth(colour_depth);
                 break;
+            }
             case 'd': {
                 std::string diff_type = optarg;
+                int (*difference_func)(Colour *, Colour *);
                 if (diff_type == "lum") {
                     difference_func = getColourLuminosityDiff;
                 } else if (diff_type == "colour" || diff_type == "color") {
@@ -143,132 +67,57 @@ int main(int argc, char *argv[]) {
                     std::cout << "Unknown difference type " << diff_type << std::endl;
                     return 1;
                 }
+                rainbow_renderer->setDifferenceFunction(difference_func);
                 break;
             }
             case 's': {
                 std::string start_type_str = optarg;
                 if (start_type_str == "center" || start_type_str == "centre") {
-                    start_type = START_TYPE_CENTRE;
+                    start_type = RainbowRenderer::START_TYPE_CENTRE;
                 } else if (start_type_str == "corner") {
-                    start_type = START_TYPE_CORNER;
+                    start_type = RainbowRenderer::START_TYPE_CORNER;
                 } else if (start_type_str == "edge") {
-                    start_type = START_TYPE_EDGE;
+                    start_type = RainbowRenderer::START_TYPE_EDGE;
                 } else if (start_type_str == "random") {
-                    start_type = START_TYPE_RANDOM;
+                    start_type = RainbowRenderer::START_TYPE_RANDOM;
                 } else {
                     std::cout << "Unknown start type " << start_type_str << std::endl;
                     return 1;
                 }
+                rainbow_renderer->setStartType(start_type);
                 break;
             }
             case '?':
                 if (optopt == 'h' || optopt == 'w' || optopt == 'c' || optopt == 'd') {
-                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                    std::cerr << "Option -" << optopt << " requires and argument" << std::endl;
                 } else if (isprint(optopt)) {
+                    std::cerr << "Unknown option -" << optopt << std::endl;
                     fprintf(stderr, "Unknown option `-%c'.\n", optopt);
                 } else {
-                    fprintf(stderr,
-                            "Unknown option character `\\x%x'.\n",
-                            optopt);
+                    std::cerr << "Unknown option character" << optopt << std::endl;
                 }
                 return 1;
 
             default:
                 abort();
         }
+    }
+
     for (; optind < argc; optind++) {
-        num_start_points = (int) strtol(argv[optind], nullptr, 0);
+        int num_start_points = (int) strtol(argv[optind], nullptr, 0);
+        if (num_start_points > 0) {
+            rainbow_renderer->setNumStartPoints(num_start_points);
+        }
     }
 
     time_t start_time = time(nullptr);
-    pixels.resize(PIXELS_WIDE * PIXELS_HIGH);
-    long COLOUR_COUNT = fillColours(COLOUR_DEPTH);
-    std::vector<Point> possible_start_points;
-    if (num_start_points <= 0) {
-        num_start_points = INT32_MAX;
-    }
-
-    switch (start_type) {
-        case START_TYPE_CENTRE: {
-            possible_start_points.emplace_back(Point(PIXELS_WIDE / 2, PIXELS_HIGH / 2));
-            break;
-        }
-        case START_TYPE_CORNER: {
-            possible_start_points.emplace_back(Point(0, 0));
-            possible_start_points.emplace_back(Point(PIXELS_WIDE - 1, 0));
-            possible_start_points.emplace_back(Point(0, PIXELS_HIGH - 1));
-            possible_start_points.emplace_back(Point(PIXELS_WIDE - 1, PIXELS_HIGH - 1));
-            break;
-        }
-        case START_TYPE_EDGE:
-        case START_TYPE_RANDOM:
-            for (int y = 0; y < PIXELS_HIGH; ++y) {
-                for (int x = 0; x < PIXELS_WIDE; ++x) {
-                    if (start_type == START_TYPE_RANDOM ||
-                        (x == 0 || x == PIXELS_WIDE - 1 || y == 0 || y == PIXELS_HIGH - 1)) {
-                        possible_start_points.emplace_back(Point(x, y));
-                    }
-                }
-            }
-            break;
-    }
-    std::shuffle(possible_start_points.begin(), possible_start_points.end(), rng);
-    for (int i = 0; i < possible_start_points.size() && i < num_start_points; ++i) {
-        fillPoint(possible_start_points[i]);
-    }
-
-    while (true) {
-        if (available_edges.empty() || colour_index == COLOUR_COUNT) {
-            std::cout << "Out of edges or colours" << std::endl;
-            break;
-        }
-        Colour *current_colour = &colours[colour_index];
-        Point best_point;
-        int best_difference = INT32_MAX;
-        for (auto &available_edge : available_edges) {
-            int diff = difference_func(current_colour, &getPixelAtPoint(available_edge)->colour);
-            if (diff < best_difference) {
-                best_point = available_edge;
-                best_difference = diff;
-            }
-        }
-
-        int neighbour_count = getNeighboursOfPoint(best_point);
-        std::shuffle(std::begin(neighbours), std::end(neighbours), rng);
-        int neighbour_index = 0;
-        for (; neighbour_index < neighbour_count; ++neighbour_index) {
-            Point &neighbour = neighbours[neighbour_index];
-            Pixel *neighbour_pixel = getPixelAtPoint(neighbour);
-            if (neighbour_pixel->is_filled) {
-                continue;
-            }
-            neighbour_pixel->colour = *current_colour;
-            neighbour_pixel->is_filled = true;
-            available_edges.push_back(neighbour);
-            ++colour_index;
-
-            if (colour_index % 1000 == 0) {
-                std::cout << colour_index << ": " << available_edges.size() << " ("
-                          << ((float) colour_index / float(PIXELS_HIGH * PIXELS_WIDE) * 100) << "%)" << std::endl;
-            }
-            break;
-        }
-
-        if (neighbour_index == neighbour_count) {
-            available_edges.remove(best_point);
-        }
-    }
+    rainbow_renderer->init();
+    rainbow_renderer->fill();
     time_t end_time = time(nullptr);
     std::cout << "Completed in " << (end_time - start_time) << "s" << std::endl;
 
-    BMP bmp = BMP(PIXELS_WIDE, PIXELS_HIGH, false);
-    for (int y = 0; y < PIXELS_HIGH; ++y) {
-        for (int x = 0; x < PIXELS_WIDE; ++x) {
-            Pixel *pixel = getPixel(x, y);
-            bmp.set_pixel(x, y, pixel->colour.b, pixel->colour.g, pixel->colour.r);
-        }
-    }
-    bmp.write("output.bmp");
+    rainbow_renderer->writeToFile("output.bmp");
 
+    delete rainbow_renderer;
     return 0;
 }
---
+++ rainbow_renderer.h
@@ -0,0 +1,216 @@
+#ifndef RAINBOW_C_RAINBOW_RENDERER_H
+#define RAINBOW_C_RAINBOW_RENDERER_H
+
+#include <vector>
+#include <list>
+
+#include "colour.h"
+#include "pixel.h"
+#include "point.h"
+
+class RainbowRenderer {
+public:
+    std::default_random_engine rng = std::default_random_engine(std::random_device{}());
+
+    enum StartType {
+        START_TYPE_CENTRE,
+        START_TYPE_CORNER,
+        START_TYPE_RANDOM,
+        START_TYPE_EDGE
+    };
+
+    void setPixelsWide(int _pixels_wide) {
+        this->pixels_wide = _pixels_wide;
+    }
+
+    void setPixelsHigh(int _pixels_high) {
+        this->pixels_high = _pixels_high;
+    }
+
+    void setNumStartPoints(int _num_start_points) {
+        this->num_start_points = _num_start_points;
+    }
+
+    void setColourDepth(int _colour_depth) {
+        this->colour_depth = _colour_depth;
+    }
+
+    void setDifferenceFunction(int (*_func)(Colour *, Colour *)) {
+        this->difference_function = _func;
+    }
+
+    void setStartType(StartType _start_type) {
+        this->start_type = _start_type;
+    }
+
+
+    int pixels_wide = 256;
+    int pixels_high = 256;
+    int colour_depth = 256;
+    int num_start_points = INT32_MAX;
+    StartType start_type = StartType::START_TYPE_CENTRE;
+
+    int (*difference_function)(Colour *, Colour *) = getColourAbsoluteDiff;
+
+    std::vector<Colour> colours;
+    std::vector<Pixel> pixels;
+    std::list<Point> available_edges;
+    std::vector<Point> neighbours;
+    long colour_index = 0;
+
+    /// Get the pixel at the given x and y coordinates
+    /// \param x The x coordinate
+    /// \param y The y coordinate
+    /// \return The pixel at that position
+    Pixel *getPixel(int x, int y) {
+        return &this->pixels[y * this->pixels_wide + x];
+    }
+
+    /// Gets the pixel at the given point
+    /// \param point The point
+    /// \return The pixel at that point
+    Pixel *getPixelAtPoint(Point &point) {
+        return &this->pixels[point.y * this->pixels_wide + point.x];
+    }
+
+    /// Gets the neighbour positions of the given point (points over the edge of the board aren ot returned)
+    /// \param point The point to find the neighbours for
+    /// \return The number of neighbours
+    int getNeighboursOfPoint(Point point) {
+        neighbours.clear();
+        for (int y = -1; y <= 1; ++y) {
+            if (y + point.y < 0 || y + point.y >= this->pixels_high) {
+                continue;
+            }
+
+            for (int x = -1; x <= 1; ++x) {
+                if ((x == 0 && y == 0) || x + point.x < 0 || x + point.x >= this->pixels_wide) {
+                    continue;
+                }
+
+                neighbours.emplace_back(Point(x + point.x, y + point.y));
+            }
+        }
+        return neighbours.size();
+    }
+
+    /// Fills the list of random colours
+    /// \param colour_depth The number of each unique colours in each channel
+    void fillColours() {
+        for (int r = 0; r < this->colour_depth; ++r) {
+            for (int g = 0; g < this->colour_depth; ++g) {
+                for (int b = 0; b < this->colour_depth; ++b) {
+                    this->colours.emplace_back(Colour(r * 255 / (this->colour_depth - 1),
+                                                      g * 255 / (this->colour_depth - 1),
+                                                      b * 255 / (this->colour_depth - 1)));
+                }
+            }
+        }
+        std::shuffle(std::begin(colours), std::end(colours), rng);
+    }
+
+
+    /// Fills the pixel at the given point
+    /// \param point The pointto place the pixel at
+    void fillPoint(Point &point) {
+        this->available_edges.push_back(point);
+        Pixel *centre_pixel = getPixelAtPoint(point);
+        centre_pixel->colour = this->colours[this->colour_index];
+        centre_pixel->is_filled = true;
+        ++this->colour_index;
+    }
+
+    void init() {
+
+        pixels.resize(this->pixels_wide * this->pixels_high);
+        this->fillColours();
+        std::vector<Point> possible_start_points;
+
+        switch (start_type) {
+            case START_TYPE_CENTRE: {
+                possible_start_points.emplace_back(Point(this->pixels_wide / 2, this->pixels_high / 2));
+                break;
+            }
+            case START_TYPE_CORNER: {
+                possible_start_points.emplace_back(Point(0, 0));
+                possible_start_points.emplace_back(Point(this->pixels_wide - 1, 0));
+                possible_start_points.emplace_back(Point(0, this->pixels_high - 1));
+                possible_start_points.emplace_back(Point(this->pixels_wide - 1, this->pixels_high - 1));
+                break;
+            }
+            case START_TYPE_EDGE:
+            case START_TYPE_RANDOM:
+                for (int y = 0; y < this->pixels_high; ++y) {
+                    for (int x = 0; x < this->pixels_wide; ++x) {
+                        if (start_type == START_TYPE_RANDOM ||
+                            (x == 0 || x == this->pixels_wide - 1 || y == 0 || y == this->pixels_high - 1)) {
+                            possible_start_points.emplace_back(Point(x, y));
+                        }
+                    }
+                }
+                break;
+        }
+        std::shuffle(possible_start_points.begin(), possible_start_points.end(), rng);
+        for (int i = 0; i < possible_start_points.size() && i < num_start_points; ++i) {
+            fillPoint(possible_start_points[i]);
+        }
+    }
+
+    void fill() {
+        while (true) {
+            if (available_edges.empty() || colour_index == this->colours.size()) {
+                std::cout << "Out of edges or colours" << std::endl;
+                break;
+            }
+            Colour *current_colour = &colours[colour_index];
+            Point best_point;
+            int best_difference = INT32_MAX;
+            for (auto &available_edge : available_edges) {
+                int diff = this->difference_function(current_colour, &getPixelAtPoint(available_edge)->colour);
+                if (diff < best_difference) {
+                    best_point = available_edge;
+                    best_difference = diff;
+                }
+            }
+
+            int neighbour_count = getNeighboursOfPoint(best_point);
+            std::shuffle(std::begin(neighbours), std::end(neighbours), rng);
+            int neighbour_index = 0;
+            for (; neighbour_index < neighbour_count; ++neighbour_index) {
+                Point &neighbour = neighbours[neighbour_index];
+                Pixel *neighbour_pixel = getPixelAtPoint(neighbour);
+                if (neighbour_pixel->is_filled) {
+                    continue;
+                }
+                neighbour_pixel->colour = *current_colour;
+                neighbour_pixel->is_filled = true;
+                available_edges.push_back(neighbour);
+                ++colour_index;
+
+                if (colour_index % 1000 == 0) {
+                    std::cout << colour_index << ": " << available_edges.size() << " ("
+                              << ((float) colour_index / float(this->pixels_high * this->pixels_wide) * 100) << "%)"
+                              << std::endl;
+                }
+                break;
+            }
+
+            if (neighbour_index == neighbour_count) {
+                available_edges.remove(best_point);
+            }
+        }
+    }
+
+    void writeToFile(std::string _filename) {
+        BMP bmp = BMP(this->pixels_wide, this->pixels_high, false);
+        for (int y = 0; y < this->pixels_high; ++y) {
+            for (int x = 0; x < this->pixels_wide; ++x) {
+                Pixel *pixel = getPixel(x, y);
+                bmp.set_pixel(x, y, pixel->colour.b, pixel->colour.g, pixel->colour.r);
+            }
+        }
+        bmp.write(_filename.c_str());
+    }
+};
+
+#endif //RAINBOW_C_RAINBOW_RENDERER_H
